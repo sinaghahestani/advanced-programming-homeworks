f200434dc9bf0bfe3d16d172ab0a488481172a6d
sinaghahestani <sinaghahestani@gmail.com>
2026-06-27 13:49:48 +0330

    Homework 1: Prime finder without functions

---
+++ Prime_finder.py
@@ -0,0 +1,12 @@
+n = 1000000
+is_prime = [True] * (n + 1)
+is_prime[0] = is_prime[1] = False
+
+for i in range(2, int(n**0.5) + 1):
+    if is_prime[i]:
+        for j in range(i*i, n + 1, i):
+            is_prime[j] = False
+
+primes = [i for i in range(n + 1) if is_prime[i]]
+print(primes)
+
